b68702b9f98ecea50ae597446f094e1d50ae49de
Johannes Hwang <ilovetrout712@yahoo.com>
2019-08-15 21:59:20 -0700

    almost launch

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 // Make it rain!
+
+$('nav a').on('click', function(e) {
+  // prevent the standard link operation on click
+  e.preventDefault();
+  // use the href of the link to identify what
+  // section to scroll to
+  var thisTarget = $(this).attr('href');
+  // get that section's top offset
+  var targetOffset = $(thisTarget).offset().top;
+  // use jQuery.animate() to animate the body's
+  // scrollTop to the targetOffest
+  $('body, html').animate({
+    scrollTop: targetOffset
+  }, 600);
+});
